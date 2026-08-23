cf184622b4ed4cc8c274396b8620a12269f72e0c
Stuart Denovan <stuart.denovan@gmail.com>
2026-08-23 15:59:33 +0100

    feat: add favicon images and site manifest; enhance hero sections in about and home pages

--- src/app/pages/about/about.component.ts
+++ src/app/pages/about/about.component.ts
@@ -1,9 +1,11 @@
 import { Component, inject } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 
 import { SiteConfigService } from '../../core/config/site-config.service';
 
 @Component({
   selector: 'app-about',
+  imports: [NgOptimizedImage],
   templateUrl: './about.component.html',
 })
 export class AboutComponent {
--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,11 +1,12 @@
 import { Component, inject } from '@angular/core';
+import { NgOptimizedImage } from '@angular/common';
 import { RouterLink } from '@angular/router';
 
 import { SiteConfigService } from '../../core/config/site-config.service';
 
 @Component({
   selector: 'app-home',
-  imports: [RouterLink],
+  imports: [NgOptimizedImage, RouterLink],
   templateUrl: './home.component.html',
 })
 export class HomeComponent {
